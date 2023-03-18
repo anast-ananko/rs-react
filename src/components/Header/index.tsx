fe5a6e053a9dd1c913cefbed6152e4f1ac7e6856
Nastassia Ananka <anast.ananko@gmail.com>
2023-03-18 22:33:03 +0100

    feat: add name of active page in header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -4,18 +4,43 @@ import { NavLink } from 'react-router-dom';
 import './header.scss';
 
 class Header extends Component {
+  state = {
+    activePage: window.location.pathname.slice(1),
+  };
+
+  handleClick = (e: React.MouseEvent<HTMLAnchorElement>) => {
+    const element = e.target as HTMLElement;
+    this.setState({
+      activePage: element.getAttribute('data-name'),
+    });
+  };
+
   render() {
     return (
-      <header className="header">
-        <div className="container">
-          <NavLink to="/" className="header__link">
-            Home
-          </NavLink>
-          <NavLink to="/about" className="header__link">
-            About
-          </NavLink>
-        </div>
-      </header>
+      <>
+        <header className="header">
+          <div className="container header__container">
+            <NavLink data-name="home" to="/" className="header__link" onClick={this.handleClick}>
+              Home
+            </NavLink>
+            <NavLink
+              data-name="about"
+              to="/about"
+              className="header__link"
+              onClick={this.handleClick}
+            >
+              About
+            </NavLink>
+            {this.state.activePage === 'home' ? (
+              <div className="header__title">Home page</div>
+            ) : this.state.activePage === 'about' ? (
+              <div className="header__title">About page</div>
+            ) : (
+              <div className="header__title">Not found page</div>
+            )}
+          </div>
+        </header>
+      </>
     );
   }
 }
